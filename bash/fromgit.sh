f420e29c7d2850b7c6f641af265818a6d8560689
Jnathan0 <55114731+Jnathan0@users.noreply.github.com>
2019-12-28 22:00:03 -0800

    Update to run without changing code
    -Changed pathways to use machines hostname instead of having to hard-code paths
    -Added colored messages to indicate various processes 
    -NOTE: the git clone url still needs to be changed according to which GIT repo you want to clone

--- bash/fromgit.sh
+++ bash/fromgit.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
-
-#NOTE: MAKE SURE TO CHANGE 'GORDON' TO YOUR PROPER HOST NAME
-#NOTE: MAKE SURE TO CHANGE PATHWAY ON LINE 10 AND 26 TO /home/YOUR_HOSTNAME/catkin_ws/src
-#NOTE: GIT LINK IN LINE 10 IS FOR THE BASESTATION REPO, CHANGE LINK ACCORDINGLY TO YOUR DESIRED REPO  
-
-cd /home/gordon
+cd /home/$HOSTNAME
 mkdir gittemp #make a folder, clone the given repo into it
 cd gittemp
 git clone https://github.com/titan-rover/base_station.git
-SRC="/home/gordon/test1/src" #change gordon to $HOSTNAME
-GIT="/home/gordon/gittemp/base_station/src"
+SRC="/home/$HOSTNAME/catkin_ws/src" #set SRC var to path of local SRC
+GIT="/home/$HOSTNAME/gittemp/base_station/src" #make temp path to temp folder
 DIFF=$(diff -r $GIT $SRC) #compare contents of local src folder to git clone using DIFF
 
 echo "Finding difference between GIT repo and local SRC ..."
 echo $DIFF 
 if [ "$DIFF" != "" ] 
 then
-    echo "---Changes in directory contents found.---"; echo #;
+    printf "\n"; echo "$(tput bel) $(tput setaf 2)---Changes in directory contents found.---$(tput sgr0)"; printf "\n";
 else
-    echo "---No changes in directories found.---";
+    printf "\n"; echo "$(tput setaf 3)---No changes in directories found.---$(tput sgr0)"; printf "\n";
 fi
 
 while true; do
-	read -p "Replace contents of local SRC with the contents of GIT folder (Y\N)?: " yn
-	case $yn in
-		[Yy]* ) rm -R /home/gordon/test1/src;mv -T $GIT $SRC; rm -R /home/gordon/gittemp/;break;; #remove old local src, move cloned src into local folder, remove temp git folder
-		[Nn]* ) echo "exiting.";rm -R /home/gordon/gittemp;exit;; #if no then remove the temp GIT clone folder
-		*) echo "Please answer Y or N.";;
-	esac
+        read -p "Replace local SRC with GIT (Y\N)? $(tput setaf 1) warning this will overwrite current SRC contents $(tput sgr0): " yn
+        case $yn in
+                [Yy]* ) rm -R /home/$HOSTNAME/catkin_ws/src;mv -T $GIT $SRC; rm -R /home/$HOSTNAME/gittemp/; printf "\n$(tput setaf 3)---Initiating ca$
+                [Nn]* ) echo "exiting.";rm -R /home/$HOSTNAME/gittemp;exit;; #if no then remove the temp GIT clone folder
+                *) echo "Please answer Y or N.";;
+        esac
 done
+
 
