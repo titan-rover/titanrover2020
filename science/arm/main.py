a4633f7e80dd3817be319a53557709de0a76d357
JOverbake <54606154+JOverbake@users.noreply.github.com>
2020-01-14 20:25:18 -0800

    Updated w/ pinout of the DC motor and limit switch

--- science/arm/main.py
+++ science/arm/main.py
@@ -4,17 +4,30 @@ import time
 
 #TODO: Add 1 DC motor and 1 limit switch
 
-                   # Pwm      # Dir      # Enab (HIGH = Disable)
-armAction = { 0 : {4 : 'X1',  5 : 'X11',  6 : 'X12'},               # CH1 TIM2    Joint 1 ===> low = CLKW,    high = CCW,     freq = 1250  (Top)
-              1 : {4 : 'X2',  5 : 'X9',  6 : 'X10'},               # CH2 TIM2    Joint 2 ===> low = DOWN,    high = UP,      freq = 1250  (Front)
-              2 : {4 : 'X3',  5 : 'X7',  6 : 'X8'}              # CH3 TIM2    Joint 3 ===> low = CLKW,    high = CCW      freq = 1250  (Top)
+                           # Pwm      # Dir      # Enab (HIGH = Disable)
+armAction = { 0 : { 0 : {4 : 'X1',  5 : 'X11',  6 : 'X12'},               # CH1 TIM2    Joint 1 ===> low = CLKW,    high = CCW,     freq = 1250  (Top)
+                    1 : {4 : 'X2',  5 : 'X9',  6 : 'X10'},                # CH2 TIM2    Joint 2 ===> low = DOWN,    high = UP,      freq = 1250  (Front)
+                    2 : {4 : 'X3',  5 : 'X7',  6 : 'X8'}                  # CH3 TIM2    Joint 3 ===> low = CLKW,    high = CCW      freq = 1250  (Top)
+                  },
+              # DC         # Pin 1     # Pin 2
+              1 : { 0 : {3 : 'Y1',  4 : 'Y2'}
+                  },
+              # Signal pin for limit switch
+              2 : { Pin('X4', Pin.OUT)}
             }
-                       # Counter Clockwise / Left                  # Clockwise / Right                          # Stop                                       # Start
-movements = { 0 : {4 : Pin(armAction[0][5], Pin.OUT_PP).high, 5 : Pin(armAction[0][5], Pin.OUT_PP).low, 6 : Pin(armAction[0][6], Pin.OUT_PP).high, 7 : Pin(armAction[0][6], Pin.OUT_PP).low},
-                       # UP                                         # Down                                      # Stop                                       # Start
-              1 : {4 : Pin(armAction[1][5], Pin.OUT_PP).high, 5 : Pin(armAction[1][5], Pin.OUT_PP).low, 6 : Pin(armAction[1][6], Pin.OUT_PP).high, 7 : Pin(armAction[1][6], Pin.OUT_PP).low},
-                       # Counter Clockwise / Left                  # Clockwise / Right                          # Stop                                       # Start
-              2 : {4 : Pin(armAction[2][5], Pin.OUT_PP).high, 5 : Pin(armAction[2][5], Pin.OUT_PP).low, 6 : Pin(armAction[2][6], Pin.OUT_PP).high, 7 : Pin(armAction[2][6], Pin.OUT_PP).low}
+                            # Counter Clockwise / Left                  # Clockwise / Right                          # Stop                                       # Start
+movements = { 0 : { 0 : {4 : Pin(armAction[0][5], Pin.OUT_PP).high, 5 : Pin(armAction[0][5], Pin.OUT_PP).low, 6 : Pin(armAction[0][6], Pin.OUT_PP).high, 7 : Pin(armAction[0][6], Pin.OUT_PP).low},
+                            # UP                                         # Down                                      # Stop                                       # Start
+                    1 : {4 : Pin(armAction[1][5], Pin.OUT_PP).high, 5 : Pin(armAction[1][5], Pin.OUT_PP).low, 6 : Pin(armAction[1][6], Pin.OUT_PP).high, 7 : Pin(armAction[1][6], Pin.OUT_PP).low},
+                            # Counter Clockwise / Left                  # Clockwise / Right                          # Stop                                       # Start
+                    2 : {4 : Pin(armAction[2][5], Pin.OUT_PP).high, 5 : Pin(armAction[2][5], Pin.OUT_PP).low, 6 : Pin(armAction[2][6], Pin.OUT_PP).high, 7 : Pin(armAction[2][6], Pin.OUT_PP).low}
+                  },
+                  #DC motor
+              1 : { 0 : {0 : Pin(fluidicsPins[1][0][4], Pin.OUT).high(),  #ON
+                         1 : Pin(fluidicsPins[1][0][4], Pin.OUT).low(),   #OFF
+                         'cmd' : 0
+                        }
+                  }
             }
 
 def getcommand():
@@ -40,6 +53,9 @@ if __name__ == "__main__":
         Pin(armAction[i][6], Pin.OUT_PP).high()
         Pin(armAction[i][5], Pin.OUT_PP).high()
         Timer(2, freq=1250).channel(i+1, Timer.PWM, pin=Pin(armAction[i][4])).pulse_width_percent(50)
+    #initialize DC pins
+    Pin(fluidicsPins[1][0][3], Pin.OUT).low()
+    Pin(fluidicsPins[1][0][4], Pin.OUT).low()
     pyb.LED(2).toggle()
     time.sleep(1)
     pyb.LED(2).toggle()
