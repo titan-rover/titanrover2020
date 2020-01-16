e8ef9e9e5eaf58ecc1804baaf9d403bbfaca5430
rpaceiv <rpaceiv@gmail.com>
2020-01-16 12:38:49 -0800

    Changed the fluidics main code to a new style, still needs to be tested on actual hardware Version(2.0)

--- science/fluidics/main.py
+++ science/fluidics/main.py
@@ -1,210 +1,180 @@
 # main.py -- put your code here!
-from pyb import Pin, Timer, UART, ADC
+from pyb import Timer, UART, ADC, Pin, ExtInt
 import pyb
 import time
-import _thread
-#import cmd
 
-fluidicsPins = { 0 : { 0 : {3 : 'X3',   #PWM - CH3 Timer 2
-                            4 : 'X9',   #DIR
-                            5 : 'X10'}, #EN
-                       1 : {3 : 'X4',   #PWM - CH4 Timer 2
-                            4 : 'X11',  #DIR
-                            5 : 'X12'}  #EN
-                     },
-                 1 : { 0 : {3 : 'Y1',   #DC Motor 1 Pin 1
-                            4 : 'Y2'},  #DC Motor 1 Pin 2
-                       1 : {3 : 'Y3',   #DC Motor 2 Pin 1
-                            4 : 'Y4'},  #DC Motor 2 Pin 2
-                       2 : {3 : 'Y5',   #DC Motor 3 Pin 1
-                            4 : 'Y6'},  #DC Motor 3 Pin 2
-                       3 : {3 : 'Y7',   #DC Motor 4 Pin 1
-                            4 : 'Y8'}   #DC Motor 4 Pin 2
-                     },
-                 2 : { 0 : {3 : 'Y9'} }
-               }
+# Virtual UART Port
+ser = pyb.USB_VCP(0)
 
-            #Steppers; 0: Stepper One, 1: Stepper Two
-fluidics = { 0 : { 0 : {0 : Pin(fluidicsPins[0][0][5], Pin.OUT_PP).high, #OFF
-                        1 : Pin(fluidicsPins[0][0][4], Pin.OUT_PP).high, #CCW
-                        2 : Pin(fluidicsPins[0][0][4], Pin.OUT_PP).low,   #CW
-                        #'cmd' : 0
-                       },
-                   1 : {0 : Pin(fluidicsPins[0][0][5], Pin.OUT_PP).high, #OFF
-                        1 : Pin(fluidicsPins[0][0][4], Pin.OUT_PP).high, #CCW
-                        2 : Pin(fluidicsPins[0][0][4], Pin.OUT_PP).low,   #CW
-                        #'cmd' : 0
-                       }
-                 }, #last is last state
-            #Motors; 0: Motor One, 1: Motor Two, 2: Motor Three, 3: Motor Four
-            #Only change the second pin to either True or False
-             1 : { 0 : {0 : Pin(fluidicsPins[1][0][4], Pin.OUT).high(),  #ON
-                        1 : Pin(fluidicsPins[1][0][4], Pin.OUT).low(),   #OFF
-                        'cmd' : 0
-                        },
-                   1 : {0 : Pin(fluidicsPins[1][1][4], Pin.OUT).high(),  #ON
-                        1 : Pin(fluidicsPins[1][1][4], Pin.OUT).low(),   #OFF
-                        'cmd' : 0
-                       },
-                   2 : {0 : Pin(fluidicsPins[1][2][4], Pin.OUT).high(),  #ON
-                        1 : Pin(fluidicsPins[1][2][4], Pin.OUT).low(),   #OFF
-                        'cmd' : 0
-                       },
-                   3 : {0 : Pin(fluidicsPins[1][3][4], Pin.OUT).high(),  #ON
-                        1 : Pin(fluidicsPins[1][3][4], Pin.OUT).low(),   #OFF
-                        'cmd' : 0
-                       }
-                 },
-             #Solenoid toggles either on or off
-             2 : { 0 : {0 : Pin(fluidicsPins[2][0][3], Pin.OUT).high(),  #ON
-                        1 : Pin(fluidicsPins[2][0][3], Pin.OUT).low(),   #OFF
-                        'cmd' : 0
-                        }
-                 }
-           }
+#Tracks the toggle state of the current device
+dc_state1 = [0]
+dc_state2 = [0]
+dc_state3 = [0]
+dc_state4 = [0]
+sol_state = [0]
 
-def run_steppers(fm_cw, fm_ccw, sm_cw, sm_ccw):
-    #check all four buttons, first_motor cw, first_motor ccw, second_motor cw, second_motor ccw
-    if fm_cw == 1:
-        #run first stepper cw
-        fluidics[0][0][2]()
-    elif fm_ccw == 1:
-        #run first stepper ccw
-        fluidics[0][0][1]()
-    elif sm_cw == 1:
-        #run second stepper cw
-        fluidics[0][1][2]()
-    elif sm_ccw == 1:
-        #run second stepper ccw
-        fluidics[0][1][1]()
+Pins = {
+        #Manafold
+        0 : {
+            0 : {
+                0 : "(Timer(2, freq=1250)).channel(3, Timer.PWM, pin=Pin('X3'))",  #PWM Pin
+                1 : Pin('X9',Pin.OUT_PP),                                          #Dir Pin
+                2 : Pin('X10',Pin.OUT_PP)                                          #ENA Pin
+                }
+            },
+        #Spectroscopy
+        1 : {
+            0 : {
+                0 : "(Timer(2, freq=1250)).channel(4, Timer.PWM, pin=Pin('X4'))",  
+                1 : Pin('X11',Pin.OUT_PP),  
+                2 : Pin('Y12',Pin.OUT_PP)
+                }
+            },             
+        #Pumps
+        2 : {
+            0 : {
+                0 : Pin('Y1',Pin.OUT_PP), #in1
+                1 : Pin('Y2',Pin.OUT_PP)  #in2
+                },
+            1 : {
+                0 : Pin('Y3',Pin.OUT_PP), #in1
+                1 : Pin('Y4',Pin.OUT_PP)  #in2
+                },
+            2 : {
+                0 : Pin('Y5',Pin.OUT_PP), #in1
+                1 : Pin('Y6',Pin.OUT_PP)  #in2
+                },
+            3 : {
+                0 : Pin('Y7',Pin.OUT_PP), #in1
+                1 : Pin('Y8',Pin.OUT_PP)  #in2
+                }
+            },
+        #Solenoid
+        3 : {
+            0 : {
+                0 : Pin('Y9',Pin.OUT_PP) #sig pin
+                }
+            }
+}
+
+# Pulse a stepper motor
+def stepperMove(stepper, dir_port, en_port, dir_val):
+    dir_port.value(dir_val)
+    stepper.pulse_width_percent(50)
+    pyb.LED(4).on()
+    stepper.pulse_width_percent(0)
+    pyb.LED(4).off()
+
+def togglePump(in1,in2,state):
+    time.sleep(.4)
+    if(state[0] == 0):
+        in1.high()
+        in2.low()
+        state[0] = 1
     else:
-        #don't run them
-        fluidics[0][0][0]()
-        fluidics[0][1][0]()
+        in1.low()
+        in2.high()
+        state[0] = 0
 
-def run_dc(f_m, s_m, t_m, fr_m):
-    #check the buttons values to figure out which one to press
-    if f_m == 1: #received first 1, but before had 0
-        fluidics[1][0][0]()
+def stopPump(in1,in2):
+    time.sleep(.4)
+    in1.low()
+    in2.low()
+
+def toggleSolenoid(sig,state):
+    time.sleep(.4)
+    if(state[0] == 0):
+        sig.high()
+        state[0] = 1
     else:
-        fluidics[1][0][1]()
+        sig.low()
+        state[0] = 0
 
-    if s_m == 1: #received first 1, but before had 0
-        fluidics[1][1][0]()
-    else:
-        fluidics[1][1][1]()
+fluidAction = {
+        0 : {
+            0 : {                     #PWM Port         #DIR Port       #ENA Port  #DIR  
+                0 : 'stepperMove(eval(Pins[0][0][0]), Pins[0][0][1], Pins[0][0][2], 1)',   #Manaf CW
+                1 : 'stepperMove(eval(Pins[0][0][0]), Pins[0][0][1], Pins[0][0][2], 0)'    #Manaf CCW
+                }
+            },
 
-    if t_m == 1: #received first 1, but before had 0
-        fluidics[1][2][0]()
-    else:
-        fluidics[1][2][1]()
+        1 : {
+            0 : {
+                0 : 'stepperMove(eval(Pins[1][0][0]), Pins[1][0][1], Pins[1][0][2], 1)',   #Spect CW
+                1 : 'stepperMove(eval(Pins[1][0][0]), Pins[1][0][1], Pins[1][0][2], 0)'    #Spect CCW
+                } 
+            },
 
-    if fr_m == 1: #received first 1, but before had 0
-        fluidics[1][3][0]()
-    else:
-        fluidics[1][3][1]()
+        2 : {
+            0 : {                       #in1        #in2       #State
+                0 : 'togglePump(Pins[2][0][0],Pins[2][0][1],dc_state1)', #Toggle pump1 
+                1 : 'stopPump(Pins[2][0][0],Pins[2][0][1])'              #Stop pum1
+                },
+            1 : {                       #in1        #in2       #State
+                0 : 'togglePump(Pins[2][1][0],Pins[2][1][1],dc_state2)', #Toggle pump2 
+                1 : 'stopPump(Pins[2][1][0],Pins[2][1][1])'              #Stop pump2
+                },
+            2 : {                       #in1        #in2       #State
+                0 : 'togglePump(Pins[2][2][0],Pins[2][2][1],dc_state3)', #Toggle pump3 
+                1 : 'stopPump(Pins[2][2][0],Pins[2][2][1])'              #Stop pump3
+                },
+            3 : {                       #in1        #in2       #State
+                0 : 'togglePump(Pins[2][3][0],Pins[2][3][1],dc_state4)', #Toggle pump4 
+                1 : 'stopPump(Pins[2][3][0],Pins[2][3][1])'              #Stop pump4
+                }
+            },
+            
+        3 : {
+            0 : {                       #Sig pin   #State
+                0 : 'toggleSolenoid(Pins[3][0][0],sol_state)'   #Toggle Solenoid 
+                }
+            }
+            
+}
 
-'''
-    #check to run first motor
-    if f_m == 1 and fluidics[1][0]['cmd'] == 0: #received first 1, but before had 0
-        fluidics[1][0][1]()
-        fluidics[1][0]['cmd'] = 1
-    elif f_m == 1 and fluidics[1][0]['cmd'] == 1: #still receving trail of 1's
-        pass
-    else:
-        fluidics[1][0][1]()
-        fluidics[1][0]['cmd'] = 0
-
-    #check to run second motor
-    if s_m == 1 and fluidics[1][1]['cmd'] == 0: #received first 1, but before had 0
-        fluidics[1][1][1]()
-        fluidics[1][1]['cmd'] = 1
-    elif s_m == 1 and fluidics[1][1]['cmd'] == 1: #still receving trail of 1's
-        pass
-    else:
-        fluidics[1][1][1]()
-        fluidics[1][1]['cmd'] = 0
-
-    #check to run third motor
-    if t_m == 1 and fluidics[1][2]['cmd'] == 0: #received first 1, but before had 0
-        fluidics[1][2][1]()
-        fluidics[1][2]['cmd'] = 1
-    elif t_m == 1 and fluidics[1][2]['cmd'] == 1: #still receving trail of 1's
-        pass
-    else:
-        fluidics[1][2][1]()
-        fluidics[1][2]['cmd'] = 0
-
-    #check to run fourth motor
-    if t_m == 1 and fluidics[1][3]['cmd'] == 0: #received first 1, but before had 0
-        fluidics[1][3][1]()
-        fluidics[1][3]['cmd'] = 1
-    elif t_m == 1 and fluidics[1][3]['cmd'] == 1: #still receving trail of 1's
-        pass
-    else:
-        fluidics[1][3][1]()
-        fluidics[1][3]['cmd'] = 0
-'''
-
-
-def run_sol(button_val):
-'''
-    if button_val == 1 and fluidics[2][0]['cmd'] == 0: #received first 1, but before had 0
-        fluidics[2][0][1]()
-        fluidics[2][0]['cmd'] = 1
-    elif button_val == 1 and fluidics[2][0]['cmd'] == 1: #still receving trail of 1's
-        pass
-    else:
-        fluidics[2][0][1]()
-        fluidics[2][0]['cmd'] = 0
-'''
-if button_val == 1: #received first 1, but before had 0
-    fluidics[2][0][0]()
-else:
-    fluidics[2][0][1]()
-
-#use below for reading the serial
-'''
-def getcommand():
-    ser = UART(6, 115200)
+def getCommand():
     while True:
         try:
             data = ser.read(1)
+            time.sleep(.1)
             if str(data.decode('utf-8')) == 's':
                 move = ser.read(4)
                 move = move.decode('utf-8')
-                if str(move[0]) == 0:
-                    if str(move[3]) == 'e':
-                        #have the fluidics movement run twice if detected
-                        #steppers
-                        steppers(move[2], move[3])
-                elif str(move[1]) == 1:
-                    if str(move[3]) == 'e':
-                        #have fluids movement run one if not stepper
-
+                if str(move[3]) == 'e':
+                    time.sleep(1)
+                    eval(fluidAction[int(move[0])][int(move[1])][int(move[2])])
         except:
             pass
-'''
-for i in range(2):
-    Pin(fluidicsPins[i][i][5], Pin.OUT_PP).high() #initialize EN to high
-    Pin(fluidicsPins[i][i][4], Pin.OUT_PP).high() #initilize DIR to high
-    Timer(2, freq=100).channel(i+3, Timer.PWM, pin=Pin(fluidicsPins[i][i][3])).pulse_width_percent(50)
 
-#initizlize both DC motor pins to False, to prevent movement
-for i in range(4):
-    Pin(fluidicsPins[1][i][3], Pin.OUT).low()
-    Pin(fluidicsPins[1][i][4], Pin.OUT).low()
 
-#initializes the solenoid to have a false value so it doesn't moves
-Pin(fluidicsPins[2][0][3], Pin.OUT).low()
+if __name__ == "__main__":
+    #Testing buttons
+    # but_top_cw = Pin('Y6', Pin.IN, Pin.PULL_NONE)
+    # but_top_ccw = Pin('Y7', Pin.IN, Pin.PULL_NONE)
+    # but_bot_ccw = Pin('Y8', Pin.IN, Pin.PULL_NONE)
+    # but_part_1 = Pin('X9', Pin.IN, Pin.PULL_NONE)
+    # but_part_2 = Pin('X10', Pin.IN, Pin.PULL_NONE)
+    # but_part_3 = Pin('X11', Pin.IN, Pin.PULL_NONE)
+    # but_vibrator = Pin('X12', Pin.IN, Pin.PULL_NONE)
 
-pyb.LED(2).toggle()
-time.sleep(1)
-pyb.LED(2).toggle()
-while True:
-    run_steppers(fm_cw, fm_ccw, sm_cw, sm_ccw)
-    run_dc(f_m, s_m, t_m, fr_m)
-    run_sol(button_val)
+    # while True:
+    #     if(but_top_cw.value()):
+    #         eval(fluidAction[0][0][0])
+    #     elif(but_top_ccw.value()):
+    #         eval(fluidAction[0][0][1])
+    #     elif(but_part_1.value()):
+    #         eval(fluidAction[1][0])
 
-#if __name__ == "__main__":
-    #first initializes steppers, sets EN to high to prevent motor from moving
+   # Setup all the ports and devices to default states
+    eval(Pins[0][0][0])
+    eval(Pins[1][0][0])
+    Pins[0][0][2].low()
+    Pins[1][0][2].low()
+
+    stopPump(Pins[2][0][0],Pins[2][0][1])
+    stopPump(Pins[2][1][0],Pins[2][1][1])
+    stopPump(Pins[2][2][0],Pins[2][2][1])
+    stopPump(Pins[2][3][0],Pins[2][3][1])
+
+    
+    getCommand()
